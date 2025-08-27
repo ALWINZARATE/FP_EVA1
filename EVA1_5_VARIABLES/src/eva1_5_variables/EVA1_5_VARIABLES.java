59186f19ab08f56cda82d2ff4ff94641723f840a
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-08-27 17:59:01 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author zarat
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //DECLARACION DE VARIABLE 
+        //crearlas las variables para que se pueda usar
+        //TIPO DE DATOS IDENTIFICADOR
+        String nombre;
+        byte edad;
+        //INICIALIZACION
+        //USAMO DE VARIABLE = --- OPERADOR DE ASIGNACION
+        //PON"RUBEN HERNANDEZ" dentro de la variable nombre
+        nombre = "Alwin zarate";
+        edad = 18;
+        //USO DE VARIABLES
+        System.out.println(nombre);
+        System.out.println(edad);
+                
+    }
+    
+}
