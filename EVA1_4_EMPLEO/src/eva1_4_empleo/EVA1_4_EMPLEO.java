8c891eff991e60bdcc00a4e0c5145198977cf6a0
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-08-26 22:12:55 -0600

    Add files via upload

---
+++ EVA1_4_EMPLEO/src/eva1_4_empleo/EVA1_4_EMPLEO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_empleo;
+
+/**
+ *
+ * @author zarat
+ */
+public class EVA1_4_EMPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Empleo:");
+        System.out.println("Ingeniero en sistemas");
+         System.out.print("Salario:");
+          System.out.println("12000");
+           System.out.print("Horas semanales:");
+            System.out.println("70");
+             System.out.print("¿Requiere ingles?");
+              System.out.println("Si");
+    }
+    
+}
