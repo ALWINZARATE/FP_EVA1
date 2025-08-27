a308881ae2ddc59155b36621554756e4257c70b7
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-08-26 22:05:46 -0600

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author zarat
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("RUBEN HERNANDEZ");//CADENA DE TEXTO
+        System.out.println(1000);//numero entero
+        System.out.println(3.1416);//numero decimal
+        System.out.println(true);//valor logico VERDADERO O FALSO
+        System.out.println('A');//caracter
+        System.out.println("A");//CADENA DE TEXTO
+    }
+       
+   
+}
