def54a99ade35d7f3d3a59c669260da33d0a4757
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-08-26 22:11:49 -0600

    Add files via upload

---
+++ EVA1_3_PRINT/src/eva1_3_print/EVA1_3_PRINT.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_print;
+
+/**
+ *
+ * @author zarat
+ */
+public class EVA1_3_PRINT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Nombre de estudiante:");
+        System.out.println("ALWIN IVAN ZARATE PEÑA");
+        System.out.print("Numero de control:");
+        System.out.println("25550655");
+        System.out.print("EDAD:");
+        System.out.println("18");
+    }
+    
+}
