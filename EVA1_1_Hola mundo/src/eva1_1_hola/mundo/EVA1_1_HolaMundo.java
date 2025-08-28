f4a72791c5110e15f13e52fff06a4b3437f5954a
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-08-27 20:47:59 -0600

    Add files via upload

---
+++ EVA1_1_Hola mundo/src/eva1_1_hola/mundo/EVA1_1_HolaMundo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola.mundo;
+
+
+public class EVA1_1_HolaMundo {
+
+    public static void main(String[] args) {
+    System.out.println("HOLA MUNDO!!");
+    System.out.println("FUNDAMENTOS DE PROGRAMACION");
+    }
+    
+}
