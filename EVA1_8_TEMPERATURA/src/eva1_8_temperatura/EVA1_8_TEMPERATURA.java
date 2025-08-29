6b4cca27199953b9bdb32c647cd80616d4541972
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-08-28 18:20:51 -0600

    Add files via upload

---
+++ EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_temperatura;
+
+/**
+ *
+ * @author zarat
+ */
+public class EVA1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double C, F;
+        
+        C = 300;
+        F = C * 1.8 + 32;
+        System.out.println("Para convertir de 300C * 1.8 + 32 a F");
+        System.out.println(F);
+    }
+    
+}
