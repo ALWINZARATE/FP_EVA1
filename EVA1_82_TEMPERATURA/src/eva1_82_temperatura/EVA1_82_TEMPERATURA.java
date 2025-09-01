95e761db533363e79896bc70644777a44f625860
ALWINZARATE <L25550655@chihuahua2.tecnm.mx>
2025-09-01 17:50:39 -0600

    Add files via upload

---
+++ EVA1_82_TEMPERATURA/src/eva1_82_temperatura/EVA1_82_TEMPERATURA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_82_temperatura;
+
+/**
+ *
+ * @author zarat
+ */
+public class EVA1_82_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         double C, K;
+        C = 150; 
+        K = C + 273.15;
+        System.out.println("150C =");
+        System.out.println(K);
+    }
+    
+}
